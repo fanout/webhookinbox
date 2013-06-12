0014b090c035b579e6fc57c76a3111a4793962ba
Justin Karneges <justin@affinix.com>
2013-06-11 23:09:13 -0700

    separate target url and inbox url

--- urls.py
+++ urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('webhookinbox.views',
 	url(r'^create/$', 'create'),
 	url(r'^i/(?P<inbox_id>[^/]+)/$', 'inbox'),
 	url(r'^i/(?P<inbox_id>[^/]+)/refresh/$', 'refresh'),
+	url(r'^i/(?P<inbox_id>[^/]+)/in/', 'hit'),
 	url(r'^i/(?P<inbox_id>[^/]+)/items/$', 'items'),
 	url(r'^i/(?P<inbox_id>[^/]+)/stream/$', 'stream'),
 )
