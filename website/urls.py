e15728c364ea6435605334c69ca3b2a2e876f718
Justin Karneges <justin@affinix.com>
2013-06-17 11:19:24 -0700

    initial django website stuff

---
+++ website/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls.defaults import patterns, url
+
+urlpatterns = patterns('webhookinbox.website.views',
+	url(r'^$', 'home'),
+	url(r'^view/(?P<inbox_id>[^/]+)/$', 'view'),
+	url(r'^about/$', 'about'),
+)
--- website/views.py
+++ website/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
+def home(request):
+	return render_to_response('website/home.html', {}, context_instance=RequestContext(request))
+
+def view(request, inbox_id):
+	return render_to_response('website/view.html', { 'inbox_id': inbox_id }, context_instance=RequestContext(request))
+
+def about(request):
+	return render_to_response('website/about.html', {}, context_instance=RequestContext(request))
