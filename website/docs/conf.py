90a6ddcc9375021fbc96f749226d23415bb00152
Raymond <raymondshah99@gmail.com>
2014-11-07 08:41:37 -0700

    changed theme name and given sidebars

--- website/docs/conf.py
+++ website/docs/conf.py
@@ -89,8 +89,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-#html_theme = 'webhookinbox'
-html_theme = 'default'
+html_theme = 'webhookinbox'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
