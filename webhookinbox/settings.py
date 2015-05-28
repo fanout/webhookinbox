e0508b10b55dc43740206b92843f8844b6ad0bc6
Justin Karneges <justin@affinix.com>
2015-05-27 23:59:05 -0700

    full path to staticfiles

--- webhookinbox/settings.py
+++ webhookinbox/settings.py
@@ -103,7 +103,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
