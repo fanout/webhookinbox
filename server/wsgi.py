de6dcf86ef4d2bea083f22b1cc2680fac2430f57
Justin Karneges <jkarneges@fastly.com>
2023-12-22 18:16:59 -0800

    don't need to wrap application for whitenoise

--- server/wsgi.py
+++ server/wsgi.py
@@ -19,8 +19,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
 from django.conf import settings
 
 if not settings.DEBUG:
-	from whitenoise import DjangoWhiteNoise
-	application = DjangoWhiteNoise(get_wsgi_application())
+	application = get_wsgi_application()
 else:
 	from dj_static import Cling
 	application = Cling(get_wsgi_application())
